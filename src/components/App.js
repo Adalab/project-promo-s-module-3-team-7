81905a75387ce953bc8e552c3958da42b6d977c0
AlmuLloret <almudenalloret@gmail.com>
2023-03-05 13:44:35 +0100

    Add autor.scss file

--- src/components/App.js
+++ src/components/App.js
@@ -5,86 +5,86 @@ import user from '../images/user.jpeg';
 function App() {
   return <div className="App">{
     
-    <div class="container">
-      <header class="header">
-        <p class="text">Proyectos Molones</p>
+    <div className="container">
+      <header className="header">
+        <p className="text">Proyectos Molones</p>
       </header>
-      <main class="main">
-        <section class="preview">
-          <img class="image" src={cover} alt="" />
+      <main className="main">
+        <section className="preview">
+          <img className="image" src={cover} alt="" />
 
-          <section class="autor">
-            <section class="info-project">
-              <p class="subtitle">Personal Project Card</p>
-              <hr class="line" />
+          <section className="autor">
+            <section className="info-project">
+              <p className="subtitle">Personal Project Card</p>
+              <hr className="line" />
 
-              <h2 class="title">Elegant Workspace</h2>
-              <p class="slogan">Diseños Exclusivos</p>
-              <p class="desc">
+              <h2 className="title">Elegant Workspace</h2>
+              <p className="slogan">Diseños Exclusivos</p>
+              <p className="desc">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                 Libero, delectus? Voluptates at hic aliquam porro ad suscipit
                 harum laboriosam saepe earum doloribus aperiam, ullam culpa
                 accusantium placeat odit corrupti ipsum!
               </p>
-              <section class="technologies">
-                <p class="text">React JS, MongoDB</p>
+              <section className="technologies">
+                <p className="text">React JS, MongoDB</p>
               </section>
             </section>
 
-            <section class="info-autor">
-              <img class="image" src={user} alt="" />
-              <p class="job">Full Stack Developer</p>
-              <p class="name">Emmelie Björklund</p>
+            <section className="info-autor">
+              <img className="image" src={user} alt="" />
+              <p className="job">Full Stack Developer</p>
+              <p className="name">Emmelie Björklund</p>
             </section>
           </section>
         </section>
 
-        <section class="form">
-          <h2 class="title">Información</h2>
+        <section className="form">
+          <h2 className="title">Información</h2>
 
-          <section class="ask-info">
-            <p class="subtitle">Cuéntanos sobre el proyecto</p>
-            <hr class="line" />
+          <section className="ask-info">
+            <p className="subtitle">Cuéntanos sobre el proyecto</p>
+            <hr className="line" />
           </section>
 
-          <fieldset class="project">
+          <fieldset className="project">
             <input
-              class="input"
+              className="input"
               type="text"
               placeholder="Nombre del proyecto"
               name="name"
               id="name"
             />
             <input
-              class="input"
+              className="input"
               type="text"
               name="slogan"
               id="slogan"
               placeholder="Slogan"
             />
             <input
-              class="input"
+              className="input"
               type="text"
               name="repo"
               id="repo"
               placeholder="Repo"
             />
             <input
-              class="input"
+              className="input"
               type="text"
               placeholder="Demo"
               name="demo"
               id="demo"
             />
             <input
-              class="input"
+              className="input"
               type="text"
               placeholder="Tecnologías"
               name="technologies"
               id="technologies"
             />
             <textarea
-              class="textarea"
+              className="textarea"
               type="text"
               placeholder="Descripción"
               name="desc"
@@ -92,21 +92,21 @@ function App() {
             ></textarea>
           </fieldset>
 
-          <section class="ask-info">
-            <p class="subtitle">Cuéntanos sobre la autora</p>
-            <hr class="line" />
+          <section className="ask-info">
+            <p className="subtitle">Cuéntanos sobre la autora</p>
+            <hr className="line" />
           </section>
 
-          <fieldset class="autor">
+          <fieldset className="autor">
             <input
-              class="input"
+              className="input"
               type="text"
               placeholder="Nombre"
               name="autor"
               id="autor"
             />
             <input
-              class="input"
+              className="input"
               type="text"
               placeholder="Trabajo"
               name="job"
@@ -114,19 +114,19 @@ function App() {
             />
           </fieldset>
 
-          <section class="buttons-img">
-            <button class="btn">Subir foto de proyecto</button>
-            <button class="btn">Subir foto de autora</button>
+          <section className="buttons-img">
+            <button className="btn">Subir foto de proyecto</button>
+            <button className="btn">Subir foto de autora</button>
           </section>
-          <section class="buttons-img">
-            <button class="btn-large" onClick="{handleClickCreateCard}">
+          <section className="buttons-img">
+            <button className="btn-large" onClick="{handleClickCreateCard}">
               Crear Tarjeta
             </button>
           </section>
 
-          <section class="card">
-            <span class=""> La tarjeta ha sido creada: </span>
-            <a href="" class="" target="_blank" rel="noreferrer"> </a>
+          <section className="card">
+            <span className=""> La tarjeta ha sido creada: </span>
+            <a href="" className="" target="_blank" rel="noreferrer"> </a>
           </section>
         </section>
       </main>
